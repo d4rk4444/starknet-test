44285b533741ca39facbb75404cc9b99d20c67fc
Lpm2021 <111304661+Lpm2021@users.noreply.github.com>
2023-09-29 13:47:43 +0500

    Update web3.js

--- tools/web3.js
+++ tools/web3.js
@@ -12,7 +12,7 @@ export const privateToAddress = (privateKey) => {
 export const privateToStarknetAddress = async(privateKey) => {
     //new Argent X account v0.2.3 :
     const argentXproxyClassHash = "0x25ec026985a3bf9d0cc1fe17326b245dfdc3ff89b8fde106542a3ea56c5a918";
-    const argentXaccountClassHash = "0x033434ad846cdd5f23eb73ff09fe6fddd568284a0fb7d1be20ee482f044dabe2";
+    const argentXaccountClassHash = "0x033434ad846cdd5f23eb73ff09fe6fddd568284a0fb7d1be20ee482f044dabe2";  # 0x03e327de1c40540b98d05cbcb13552008e36f0ec8d61d46956d2f9752c294328
 
     //const starkKeyPairAX = ec.getKeyPair(privateKey);
     //const starkKeyPubAX = ec.getStarkKey(starkKeyPairAX); 
